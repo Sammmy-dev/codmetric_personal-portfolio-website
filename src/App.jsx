4f413433b07a20d4588b65482f001ed231ae97a5
Samuel <ajiboyeadebayo901@gmail.com>
2025-04-08 22:58:55 +0100

    Created the hero section

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,40 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='container'>
+      <nav>
+        <div className="nav-left">
+          <img src="https://via.placeholder.com/40" alt="Avatar" />
+          <div>
+            <h3>Andy Hooke</h3>
+            <div className='status'>
+              <div className="online"></div>
+              <p>Available for hire</p>
+            </div>
+          </div>
+        </div>
+        <div className="nav-links">
+          <a href="#">Why me</a>
+          <a href="#">Work</a>
+          <a href="#">Process</a>
+          <a href="#">FAQs</a>
+          <a href="#" style={{color: "#007bff", fontWeight: 600}}>Work with me</a>
+        </div>
+      </nav>
+
+      <section className="hero">
+        <h1>High-end Webflow<br />design & development</h1>
+        <p>Agency-quality Webflow websites with the personal touch of a freelancer.</p>
+        <div className="hero-buttons">
+          <button className="btn-primary">Work with me</button>
+          <button className="btn-secondary">Learn more</button>
+        </div>
+      </section>
+      <section>
+          <p>2+ years of development experience</p>
+      </section>
+  </div>
   )
 }
 
