e879b144bb074e87ca26ff07abb7a0de98eb1ddb
Samuel <ajiboyeadebayo901@gmail.com>
2025-04-28 00:43:02 +0100

    navbar complete, link and img added, and content update

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,11 @@ import { MdOutlineLocationOn,MdOutlineWhatsapp } from "react-icons/md";
 import { FaXTwitter } from "react-icons/fa6";
 import { FaGithub,FaLinkedinIn } from "react-icons/fa";
 import { LuMenu } from "react-icons/lu";
+import { useState } from 'react';
 
 
 function App() {
+  const [toggleOn, setToggleOn] = useState(false)
   const sliders = [
     {
       id:"slider1",
@@ -32,7 +34,7 @@ function App() {
       <div className="nav-hero">
         <nav>
           <div className="nav-left">
-            <img src="/Blue White Simple Professional Recruitment LinkedIn Profile Picture_20250416_183825_0000~2.png" alt="Avatar" />
+            <img src="/displayPics.png" alt="Avatar" />
             <div>
               <h3>Ajiboye Samuel</h3>
               <div className='status'>
@@ -41,15 +43,9 @@ function App() {
               </div>
             </div>
           </div>
-          <div className='menu'><LuMenu /></div>
+          <div className='menu' onClick={()=>setToggleOn((prev)=>!prev)}><LuMenu /></div>
           <div className="nav-links">
             <a href="#">About Me</a>
-            <a href="#">Projects</a>
-            <a href="#">Skills</a>
-            <a href="#">Contact</a>
-          </div>
-          <div className="mobile-nav-links">
-            <a href="#about">About Me</a>
             <a href="#">Projects</a>
             <a href="#">Skills</a>
             <a href="#">Contact</a>
@@ -57,15 +53,21 @@ function App() {
         </nav>
 
         <section className="hero">
-          <h1>High-end Webflow<br />design & development</h1>
-          <p>Agency-quality Webflow websites with the personal touch of a freelancer.</p>
+          <h1>Modern Full Stack<br />Web Development & Design</h1>
+          <p>Building responsive, Well optimized web applications with precision and creativity, hence making every pixel awesome.</p>
           <div className="hero-buttons">
-            <button className="btn-primary">Work with me</button>
-            <button className="btn-secondary">Download CV</button>
+            <a href="mailto:ajiboyeadebayo901@gmail.com"><button className="btn-primary">Work with me</button></a>
+            <a href="https://asset.cloudinary.com/dl9mx2tvo/e4b2dcb2413b0f70942434a85998e751" target='blank'><button className="btn-secondary">Download CV</button></a>
+          </div>
+          <div className="mobile-nav-links" id={toggleOn ? 'show' : 'hidden'}>
+            <a href="#about" onClick={()=>setToggleOn((prev)=>!prev)}>About Me</a>
+            <a href="#project" onClick={()=>setToggleOn((prev)=>!prev)}>Projects</a>
+            <a href="#skill" onClick={()=>setToggleOn((prev)=>!prev)}>Skills</a>
+            <a href="#contact" onClick={()=>setToggleOn((prev)=>!prev)}>Contact</a>
           </div>
         </section>
       </div>
-      <section className='second-section'>
+      <section className='second-section' id='skill'>
           <p>2+ years of development experience</p>
           <div className='slider-container'>
             {sliders.map((slider)=>{
@@ -76,7 +78,7 @@ function App() {
       <section id='about' className='about'>
         <div className="profile-cont">
           <div className='profile'>
-            <img src="/Blue White Simple Professional Recruitment LinkedIn Profile Picture_20250416_183825_0000~2.png" alt="my picture" />
+            <img src="/displayPics.png" alt="my picture" />
           </div>
           <p>Results-driven Full Stack Developer.</p>
           <p>
@@ -90,14 +92,14 @@ function App() {
           </p>
         </div>
       </section>
-      <section className="project">
+      <section className="project" id='project'>
 
         <div className='project-card'>
           <div>
             <p><span>Movie search</span><span className='link'><BiLinkAlt /></span></p>
           </div>
           <div>
-            <img src="https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+            <img src="/project-1.png" alt="" />
           </div>
         </div>
 
@@ -106,21 +108,21 @@ function App() {
             <p><span>Country search</span><span className='link'><BiLinkAlt /></span></p>
           </div>
           <div>
-            <img src="https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+            <img src="project-2.png" alt="" />
           </div>
         </div>
 
       </section>
 
-      <footer id="footer">
+      <footer id="contact">
         <div>
             <h4>Get in touch</h4>
-            <p><a href="mailto:ajiboyesamueladebayo@gmail.com"><span>ajiboyeadebayo901@gmail.com</span> <span><BiLinkAlt /></span></a></p>
+            <p><a href="mailto:ajiboyeadebayo901@gmail.com"><span>ajiboyeadebayo901@gmail.com</span> <span><BiLinkAlt /></span></a></p>
         </div>
         <div>
             <a href="https://twitter.com/Ajiboye_sat"><FaXTwitter /></a>
-            <a href="https://instagram.com/ajiboye_adebayo.s"><FaGithub /></a>
-            <a href=""><FaLinkedinIn /></a>
+            <a href="https://github.com/Sammmy-dev"><FaGithub /></a>
+            <a href="https://www.linkedin.com/in/samuel-ajiboye-35733330a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><FaLinkedinIn /></a>
             <a href="https://wa.me/+2349154173193"><MdOutlineWhatsapp /></a>
         </div>
     </footer>
